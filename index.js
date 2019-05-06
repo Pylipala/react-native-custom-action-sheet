007cb834438ca4c09f674775b969a3436e696435
Pylipala <liulunqq@163.com>
2019-05-06 20:48:38 +0800

    修复半透明遮罩不显示的问题

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Modal, StyleSheet, TouchableOpacity, View, Dimensions } from 'react-native';
+import { Modal, StyleSheet, TouchableOpacity, View, Dimensions, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import Button from './button';
 import FadeInView from './fade_in_view';
@@ -7,54 +7,66 @@ import FadeInView from './fade_in_view';
 const window = Dimensions.get('window');
 
 class ActionModal extends Component {
-  render() {
-    const {
-      modalVisible, backgroundColor, children, onCancel, buttonText,
-    } = this.props;
+    render() {
+        const {
+            modalVisible, backgroundColor, children, onCancel, buttonText,
+        } = this.props;
 
-    return (
-      <View>
-        {modalVisible && (
-          <FadeInView visible={modalVisible} backgroundColor={backgroundColor}>
-            <Modal
-              animationType="slide"
-              transparent
-              visible={modalVisible}
-              onRequestClose={onCancel}
-            >
-              <View style={styles.modalContainer}>
-                <TouchableOpacity style={styles.container} onPress={onCancel} />
-                {children}
-                <Button style={styles.button} onPress={onCancel} text={buttonText || 'Cancel'} />
-              </View>
-            </Modal>
-          </FadeInView>
-        )}
-      </View>
-    );
-  }
+        return (
+            <View style={styles.overlay}>
+              <FadeInView visible={modalVisible}>
+                {modalVisible && (
+                    <View>
+                      <Modal
+                          animationType="slide"
+                          transparent
+                          visible={modalVisible}
+                          onRequestClose={onCancel}
+                      >
+                        <View style={styles.modalContainer}>
+                          <TouchableOpacity style={styles.container} onPress={onCancel} />
+                          <View style={{backgroundColor: 'white'}}>
+                              {children}
+                            <Button style={styles.button} onPress={onCancel} text={buttonText || 'Cancel'} />
+                          </View>
+                        </View>
+                      </Modal>
+                    </View>
+                )}
+              </FadeInView>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  button: {
-    paddingTop: 30,
-  },
-  modalContainer: {
-    flex: 1,
-    padding: 8,
-    paddingBottom: 20,
-    justifyContent: 'flex-end',
-  },
+    container: {
+        flex: 1,
+    },
+    button: {
+        paddingTop: 30,
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    overlay: {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: window.height,
+        width: window.width,
+        position: 'absolute',
+        elevation: 9999999999,
+    },
 });
 
 ActionModal.propTypes = {
-  modalVisible: PropTypes.bool.isRequired,
-  onCancel: PropTypes.func.isRequired,
-  backgroundColor: PropTypes.string,
-  buttonText: PropTypes.string,
+    modalVisible: PropTypes.bool.isRequired,
+    onCancel: PropTypes.func.isRequired,
+    backgroundColor: PropTypes.string,
+    buttonText: PropTypes.string,
 };
 
 export default ActionModal;
